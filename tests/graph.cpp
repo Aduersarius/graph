a3530a22444aba606f104a4e1fb2b7dd6c6a87b3
Aduersarius <31540232+Aduersarius@users.noreply.github.com>
2018-06-17 13:56:46 +0300

    Create graph.cpp

---
+++ tests/graph.cpp
@@ -0,0 +1,34 @@
+#include <catch.hpp>
+#include <sstream>
+#include "graph.hpp"
+using namespace std;
+
+TEST_CASE(" read and DFS "){
+    graph g;
+    string input {
+	"4\n"
+        "0 1 1 0\n"
+        "0 0 1 0\n"
+        "1 0 0 1\n"
+        "0 0 0 1\n"
+    };
+    istringstream stream (input);
+    g.read(stream);
+    string result = "2 0 1 3 ";
+    ostringstream output;
+    g.DFS(2, output);
+    REQUIRE(result == output.str());
+}
+
+TEST_CASE(" invalid input "){
+    graph g;
+    string input {
+	"4\n"
+        "0 1 a 1\n"
+        "1 0 1 0\n"
+        "0 1 0 0\n"
+        "1 0 0 0\n"
+    };
+    istringstream stream (input);
+    REQUIRE(g.read(stream) == 0);
+}
